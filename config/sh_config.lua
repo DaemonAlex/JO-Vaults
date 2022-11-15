6daadf4fef6a65471010afd93d4122d0f641374d
JarretOnline <90429932+JarretOnline@users.noreply.github.com>
2022-11-14 17:41:44 -0700

    Add files via upload

---
+++ config/sh_config.lua
@@ -0,0 +1,64 @@
+
+Config = {}
+
+
+Config.StorageDefaultWeight=600000
+Config.StorageAddStep=200000
+Config.StorageAddPrice=2000
+Config.StorageSlots=200
+Config.StorageCreationAmount=5000
+
+Config.passwordSettingCost=500
+
+Config.SaleAvailableForJob=false
+Config.jobName="realestate"
+
+Config.location = {
+
+    [1] = {name="Storage01" , coords= vector3(451.03, -598.51, 28.7),heading=264.92,minz=26.36 , maxz=30.87834,length=2,width=3,distance=2.5,debug=false},
+    [2] = {name="Storage02" , coords= vector3(450.72, -601.93, 28.7),heading=264.33,minz=26.36 , maxz=30.87834,length=2,width=3,distance=2.5,debug=false},
+    [3] = {name="Storage03" , coords= vector3(450.47, -604.95, 28.7),heading=264.33,minz=26.36 , maxz=30.87834,length=2,width=3,distance=2.5,debug=false},
+    [4] = {name="Storage04" , coords= vector3(450.16, -608.35, 28.7),heading=264.33,minz=26.36 , maxz=30.87834,length=2,width=3,distance=2.5,debug=false},
+    [5] = {name="Storage05" , coords= vector3(449.92, -611.74, 28.7),heading=264.33,minz=26.36 , maxz=30.87834,length=2,width=3,distance=2.5,debug=false},
+    [6] = {name="Storage06" , coords= vector3(449.58, -615.03, 28.7),heading=264.33,minz=26.36 , maxz=30.87834,length=2,width=3,distance=2.5,debug=false},
+    [7] = {name="Storage07" , coords= vector3(449.26, -618.68, 28.7),heading=264.33,minz=26.36 , maxz=30.87834,length=2,width=3,distance=2.5,debug=false},
+    [8] = {name="Storage08" , coords= vector3(445.25, -597.77, 28.7),heading=85.6,minz=26.36 , maxz=30.87834,length=2,width=3,distance=2.5,debug=false},
+    [9] = {name="Storage09" , coords= vector3(444.67, -604.34, 28.7),heading=85.6,minz=26.36 , maxz=30.87834,length=2,width=3,distance=2.5,debug=false},
+    [10] = {name="Storage10" , coords= vector3(444.09, -610.9, 28.7),heading=85.6,minz=26.36 , maxz=30.87834,length=2,width=3,distance=2.5,debug=false},
+    [11] = {name="Storage11" , coords= vector3(443.42, -617.81, 28.7),heading=85.6,minz=26.36 , maxz=30.87834,length=2,width=3,distance=2.5,debug=false},
+    [12] = {name="Storage12" , coords= vector3(442.22, -631.96, 28.7),heading=85.6,minz=26.36 , maxz=30.87834,length=2,width=3,distance=2.5,debug=false},
+    [13] = {name="Storage13" , coords= vector3(441.96, -635.33, 28.7),heading=85.6,minz=26.36 , maxz=30.87834,length=2,width=3,distance=2.5,debug=false},
+    [14] = {name="Storage14" , coords= vector3(441.65, -638.61, 28.7),heading=85.6,minz=26.36 , maxz=30.87834,length=2,width=3,distance=2.5,debug=false},
+    [15] = {name="Storage15" , coords= vector3(441.37, -642.02, 28.7),heading=85.6,minz=26.36 , maxz=30.87834,length=2,width=3,distance=2.5,debug=false},
+    [16] = {name="Storage16" , coords= vector3(441.09, -645.27, 28.7),heading=85.6,minz=26.36 , maxz=30.87834,length=2,width=3,distance=2.5,debug=false},
+    [17] = {name="Storage17" , coords= vector3(440.8, -648.44, 28.7),heading=85.6,minz=26.36 , maxz=30.87834,length=2,width=3,distance=2.5,debug=false},
+    [18] = {name="Storage18" , coords= vector3(440.52, -651.8, 28.7),heading=85.6,minz=26.36 , maxz=30.87834,length=2,width=3,distance=2.5,debug=false},
+    [19] = {name="Storage19" , coords= vector3(446.28, -652.52, 28.7),heading=261.76,minz=26.36 , maxz=30.87834,length=2,width=3,distance=2.5,debug=false},
+    [20] = {name="Storage20" , coords= vector3(446.58, -649.08, 28.7),heading=261.76,minz=26.36 , maxz=30.87834,length=2,width=3,distance=2.5,debug=false},
+    [21] = {name="Storage21" , coords= vector3(446.9, -645.93, 28.7),heading=261.76,minz=26.36 , maxz=30.87834,length=2,width=3,distance=2.5,debug=false},
+    [22] = {name="Storage22" , coords= vector3(447.16, -642.51, 28.7),heading=261.76,minz=26.36 , maxz=30.87834,length=2,width=3,distance=2.5,debug=false},
+    [23] = {name="Storage23" , coords= vector3(447.53, -639.31, 28.7),heading=261.76,minz=26.36 , maxz=30.87834,length=2,width=3,distance=2.5,debug=false},
+    [24] = {name="Storage24" , coords= vector3(447.77, -636.04, 28.7),heading=261.76,minz=26.36 , maxz=30.87834,length=2,width=3,distance=2.5,debug=false},
+    [25] = {name="Storage25" , coords= vector3(448.05, -632.31, 28.7),heading=261.76,minz=26.36 , maxz=30.87834,length=2,width=3,distance=2.5,debug=false},
+    -----------------------
+    [26] = {name="Storage26" ,  coords= vector3(1395.47, 3616.04, 38.94),heading=335.09,minz=38 , maxz=40,length=2,width=2,distance=2,debug=false},
+    [27] = {name="Storage27" ,  coords= vector3(-53.03, -1216.57, 28.71),heading=272.79,minz=24 , maxz=30,length=2,width=2,distance=2,debug=false},
+    [28] = {name="Storage28" ,  coords= vector3(169.21, -233.03, 50.06),heading=346.89,minz=49 , maxz=51,length=2,width=2,distance=2,debug=false},
+    [29] = {name="Storage29" ,  coords= vector3(376.81, -823.9, 29.3), heading=183.53,minz=28 , maxz=30,length=2,width=1,distance=2,debug=false},
+    [30] = {name="Storage30" ,  coords= vector3(25.38, -1101.82, 29.13), heading=348.3,minz=28 , maxz=30,length=2,width=1,distance=2,debug=false}, --ammunation
+    [31] = {name="Storage31" ,  coords= vector3(183.98, -245.08, 54.07), heading=339.63,minz=53 , maxz=55,length=2,width=1,distance=2,debug=false},
+    [32] = {name="Storage32" ,  coords= vector3(-30.68, -1111.1, 26.42), heading=248.21,minz=25 , maxz=27,length=2,width=1,distance=2,debug=false},
+    [33] = {name="Storage33" ,  coords= vector3(-1475.98, -46.48, 62.02), heading=248.21,minz=61 , maxz=64,length=2,width=1,distance=2,debug=false},
+    [34] = {name="Storage34" ,  coords= vector3(-1574.6, -982.55, 13.08), heading=320.8,minz=12 , maxz=14,length=2,width=1,distance=2,debug=false},
+    [35] = {name="Storage35" ,  coords= vector3(809.66, -2163.35, 29.62), heading=320.8,minz=0 , maxz=40,length=2,width=1,distance=2,debug=false}, --ammunation2
+    [36] = {name="Storage36",   coords= vector3(883.2, -2100.54, 30.46), heading=357.8,minz=28 , maxz=32,length=2,width=2,distance=2,debug=false}, --East Customs
+    [37] = {name="Storage37",   coords= vector3(-345.34, -109.15, 39.01), heading=87.85,minz=38 , maxz=44,length=2,width=2,distance=2,debug=false}, --LS Customs
+    [38] = {name="CRYPTO",      coords= vector3(2013.63, 3030.79, -73.51), heading=32.27,minz=-75 , maxz=-70,length=2,width=2,distance=2,debug=false}, -- CryptoMining
+    [39] = {name="PUBLIC01",      coords= vector3(-72.24, -1233.72, 29.02), heading=42.97,minz=28 , maxz=30,length=2,width=2,distance=2,debug=false}, -- PUBLIC
+    [40] = {name="PUBLIC02",      coords= vector3(-66.05, -1239.17, 29.03), heading=231.77,minz=28 , maxz=30,length=2,width=2,distance=2,debug=false}, -- PUBLIC
+    [41] = {name="PUBLIC03",      coords= vector3(-56.61, -1229.02, 28.79), heading=219.06,minz=28 , maxz=30,length=2,width=2,distance=2,debug=false}, -- PUBLIC
+    [42] = {name="PUBLIC04",      coords= vector3(-53.03, -1216.5, 28.71), heading=267.40,minz=28 , maxz=30,length=2,width=2,distance=2,debug=false}, -- PUBLIC
+    [42] = {name="PUBLIC05",      coords= vector3(-65.45, -1211.94, 28.4), heading=139.25,minz=28 , maxz=30,length=2,width=2,distance=2,debug=false}, -- PUBLIC
+
+
+}
